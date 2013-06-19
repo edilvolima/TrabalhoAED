853d07d793467c4555e094fe005ad1b56076ef8e
hiagohubert <hiago_hubert@hotmail.com>
2013-06-19 01:13:44 -0300

    Update tabuleiro.c

--- tabuleiro.c
+++ tabuleiro.c
@@ -1,7 +1,15 @@
 #include "tabuleiro.h"
 #include  "lista.h"
 
-void cria_tab(){
+ void cria_tab(int altura, int largura, char alvo){
+    Tabuleiro tab = (Tabuleiro *) malloc(sizeof (Tabuleiro)); 
+    tab->altura = altura;
+    tab->largura = largura;
+    tab->alvo = alvo;
+    tab->blocos = NULL;
+    tab->mov = NULL;
     
 }
+  
+
 
